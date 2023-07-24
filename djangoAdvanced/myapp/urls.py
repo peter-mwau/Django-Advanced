3aad3d75207e4fc54019aa350e910f8636f9587c
Peter Kyale <kyalepeter2000@gmail.com>
2023-07-24 10:04:50 +0300

    Added a new template

--- djangoAdvanced/myapp/urls.py
+++ djangoAdvanced/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from . views import Home
 
 urlpatterns = [
-
+    path('', view=Home, name='home'),
 ]
--- djangoAdvanced/myapp/views.py
+++ djangoAdvanced/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def Home(request):
+    return render(request, "index.html")
