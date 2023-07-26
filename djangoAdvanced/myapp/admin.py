388a0eae91c86619c72318271b67933bca13f44d
Peter Kyale <kyalepeter2000@gmail.com>
2023-07-26 11:48:20 +0300

    renamed dbase table

--- djangoAdvanced/myapp/admin.py
+++ djangoAdvanced/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import User_Details
+from . models import User_Detail
 
 # Register your models here.
-admin.site.register(User_Details)
+admin.site.register(User_Detail)
