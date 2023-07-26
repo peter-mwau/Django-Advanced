3bb53a69ed0f7e6073ac13719d217684cb30362a
Peter Kyale <kyalepeter2000@gmail.com>
2023-07-26 10:52:21 +0300

    modified register page authentication

--- djangoAdvanced/myapp/views.py
+++ djangoAdvanced/myapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 import requests
+import json
+# from django.contrib.auth.models import authenticate
+from django.contrib.auth import login, logout
+from .models import User
 
 # Create your views here.
 
@@ -7,4 +11,24 @@ def Home(request):
     return render(request, "index.html")
 
 def Register(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+        user_exists=User.objects.filter(username__iexact=username).first()
+        if user_exists:
+            data = {
+                "error": "Username already exists!"
+            }
+        else:
+            user = User.objects.create(username=username, password=password, email=email)
+            user.save()
+            data = {
+                "success": "User created successfully!"
+            }
+    else:
+        data = {
+            "error": "Invalid request method!"
+        }
+        
+    return render(request, "register.html", data)
