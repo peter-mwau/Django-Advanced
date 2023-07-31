5b0e1bbb61ce9e9dd31aad68c13dc24e540f89fc
Peter Kyale <kyalepeter2000@gmail.com>
2023-07-31 09:27:48 +0300

    disctinctive labels in class admin

--- djangoAdvanced/myapp/admin.py
+++ djangoAdvanced/myapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . models import User_Detail
 
-# Register your models here.
-admin.site.register(User_Detail)
+class User_DetailAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password', 'email')
+
+
+admin.site.register(User_Detail, User_DetailAdmin)
